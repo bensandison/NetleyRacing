faaa31c91fe5505dd519517fd83c98b9ca33d58d
bensandison <56243297+bensandison@users.noreply.github.com>
2021-01-25 03:03:46 +0000

    implemented react native paper and created sign on lists

--- App.js
+++ App.js
@@ -1,66 +1,69 @@
 import React from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import {NavigationContainer, ThemeProvider} from "@react-navigation/native"
-import SignOn from "./SignOn.js"
-import MyResults from "./MyResults"
-import Settings from "./Settings"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from "@react-navigation/native";
+import { Provider as PaperProvider } from "react-native-paper";
 import Ionicons from 'react-native-vector-icons/Ionicons';   //import vector icons
+import SignOn from "./SignOn.js";
+import MyResults from "./MyResults";
+import Settings from "./Settings";
 
 const Tab = createBottomTabNavigator();  //outside fn as it only needs to happen once
 
 export default function MyTabs() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="SignOn"
-        backBehavior="history"
-        tabBarOptions={{
-          inactiveTintColor: "gray",
-          activeTintColor: "blue",
-          labelPosition: 'below-icon',
-          keyboardHidesTabBar: "true",
-          style: {
-            height: 60,
-            backgroundColor: 'white',
-          },
-          labelStyle: {
-            fontSize: 12,
-            marginBottom: 6,
-          }
-        }}
-      >
-        <Tab.Screen 
-          name="SignOn" 
-          component={SignOn}
-          options={{
-            tabBarLabel: 'Sign On',
-            tabBarIcon: ({color, size }) => (
-              <Ionicons name="book-outline" color={color} size={size} />
-            ),
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="SignOn"
+          backBehavior="history"
+          tabBarOptions={{
+            inactiveTintColor: "gray",
+            activeTintColor: "blue",
+            labelPosition: 'below-icon',
+            keyboardHidesTabBar: "true",
+            style: {
+              height: 60,
+              backgroundColor: 'white',
+            },
+            labelStyle: {
+              fontSize: 12,
+              marginBottom: 6,
+            }
           }}
-        />
-        <Tab.Screen 
-          name="MyResults" 
-          component={MyResults}
-          options={{
-            tabBarLabel: 'My Results',
-            tabBarIcon: ({color, size }) => (
-              <Ionicons name="eye-outline" color={color} size={size} />
-            ),
-            //tabBarBadge: 1, //may want to add this for dealing with notifications
-          }}
-        />
-        <Tab.Screen 
-          name="Settings" 
-          component={Settings}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({color, size }) => (
-              <Ionicons name="settings-outline" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen 
+            name="SignOn" 
+            component={SignOn}
+            options={{
+              tabBarLabel: 'Sign On',
+              tabBarIcon: ({color, size }) => (
+                <Ionicons name="book-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen 
+            name="MyResults" 
+            component={MyResults}
+            options={{
+              tabBarLabel: 'My Results',
+              tabBarIcon: ({color, size }) => (
+                <Ionicons name="eye-outline" color={color} size={size} />
+              ),
+              //tabBarBadge: 1, //may want to add this for dealing with notifications
+            }}
+          />
+          <Tab.Screen 
+            name="Settings" 
+            component={Settings}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({color, size }) => (
+                <Ionicons name="settings-outline" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
--- SignOn.js
+++ SignOn.js
@@ -1,10 +1,45 @@
-import React from "react"
-import {StyleSheet, View, Text} from "react-native"
+import React from "react";
+import {StyleSheet, View} from "react-native";
+import { ActivityIndicator, Divider, Text, List } from 'react-native-paper';
 
 export default function SignOn() {
     return (
-        <View style={styles.container}>
-            <Text>Sign On Here</Text>
+        <View>
+            <List.Section
+                title="Summer Series"
+                titleStyle={{fontSize : 20, fontWeight : "bold", color:"#222"}}
+            >
+                <List.Item 
+                    title="Race One"
+                    description="17 June"
+                    left={() => <List.Icon icon="flag-outline" />}
+                    onPress={() => console.log("Race 1 Selected")}
+                />
+                <List.Item
+                    title="Race One"
+                    description="18 June"
+                    left={() => <List.Icon color="#000" icon="flag-outline" />}
+                    onPress={() => console.log("Race 2 Selected")}
+                />
+            </List.Section>
+            <Divider/>
+            <List.Section
+                title="Reggatta"
+                titleStyle={{fontSize : 20, fontWeight : "bold", color:"#222"}}
+            >
+                <List.Item 
+                    title="Race One"
+                    description="21 August"
+                    left={() => <List.Icon icon="flag-outline" />}
+                    onPress={() => console.log("Race 1 Selected")}
+                />
+                <List.Item
+                    title="Race One"
+                    description="22 August"
+                    left={() => <List.Icon color="#000" icon="flag-outline" />}
+                    onPress={() => console.log("Race 2 Selected")}
+                />
+            </List.Section>
         </View>
     )
 }
