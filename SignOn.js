2827e2bac586348f9858f09260abb3fc38dd574b
bensandison <56243297+bensandison@users.noreply.github.com>
2021-01-25 17:48:46 +0000

    implemented scrolling list of races from a JSON file

--- SignOn.js
+++ SignOn.js
@@ -1,42 +1,26 @@
 import React from "react";
-import {StyleSheet, View} from "react-native";
+import { render } from "react-dom";
+import {StyleSheet, FlatList} from "react-native";
 import { Divider, List } from 'react-native-paper';
 import RaceListItem from "./components/RaceListItem"
-
-const Races = {
-    "Summer Series" : ["17 June", "18 June", "19 June"],
-    "Regatta" : ["20 Aug", "21 Aug", "22 Aug"],
-};
+import races from "./data/races.json"
 
 export default function SignOn() {
     return (
-        <View>
-            <List.Section
-                title="Summer Series"
-                titleStyle={{fontSize : 20, fontWeight : "bold", color:"#222"}}
-            >
-                <RaceListItem raceName="Race One" raceDate = "17 June" iconColor = "red"/>
-                <RaceListItem raceName="Race Two" raceDate = "17 June" iconColor = "red"/>
-                <RaceListItem raceName="Race Three" raceDate = "17 June" iconColor = "red"/>
-            </List.Section>
-            <Divider/>
-            <List.Section
-                title="Regatta"
-                titleStyle={{fontSize : 20, fontWeight : "bold", color:"#222"}}
-            >
-            <RaceListItem raceName="Race One" raceDate = "22 Aug" iconColor = "blue"/>
-                <RaceListItem raceName="Race Two" raceDate = "23 Aug" iconColor = "blue"/>
-                <RaceListItem raceName="Race Three" raceDate = "24 Aug" iconColor = "blue"/>
-            </List.Section>
-        </View>
-    )
+        <FlatList style={styles.container}
+            data={races}
+            renderItem={({item}) => {
+                return (
+                    <RaceListItem key={item.id} raceName={item.name} raceDate={item.date} iconColor="red"/>
+                )
+            }}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
     },
 })
