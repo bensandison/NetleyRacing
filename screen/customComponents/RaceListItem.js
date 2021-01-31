c8520664af213bc8c4b1dc8fe4f6af3a44dcff2b
bensandison <56243297+bensandison@users.noreply.github.com>
2021-01-31 04:58:24 +0000

    added sign on form

--- screen/customComponents/RaceListItem.js
+++ screen/customComponents/RaceListItem.js
@@ -3,13 +3,14 @@ import { List} from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
 export default function RaceListItem( {raceName = "N/A", raceDate = "N/A", iconColor = "#000"}) {  //function to display races (icon colour changes per series)
-    const navigation = useNavigation(); //TODO: give each race an ID to be identified by after navigation
+    const navigation = useNavigation();
     return(    
         <List.Item
             title={raceName}    //may also want to add some indication if the user is already signed up
             description={raceDate}
             left={() => <List.Icon icon="flag-outline" color={iconColor} />}
-            onPress={() => navigation.navigate('SignOnForm')}    //onPress could be determined by race name or we could pass a function / ID (include a default on press function in props so it wont break)
+            onPress={() => {navigation.navigate('SignOnForm', {"raceName" : raceName, "raceDate" : raceDate,}); //TODO: pass in race series name
+            }}    //on press passes name and date through navigation so we can determine the race
         />
     )
 }
--- screen/stackNavScreens/SignOnForm.js
+++ screen/stackNavScreens/SignOnForm.js
@@ -1,27 +1,54 @@
-import React from "react"
-import {StyleSheet, View, Text, SafeAreaView} from "react-native"
-import { Platform, StatusBar } from "react-native";
+import React, { useState, useContext, useEffect } from "react";
+import {StyleSheet, SafeAreaView} from "react-native";
+import { Platform, StatusBar, Text } from "react-native";
 import { TextInput, Button } from 'react-native-paper';
+import { ScrollView } from "react-native-gesture-handler";
 
 import AppBarComponent from "../customComponents/appBarComponent";
-import CustomHeader from '../customComponents/customHeader'
+import AppContext from '../customComponents/appContext';
+import CustomSubHeader from '../customComponents/customSubHeader';
 
-export default function SignOnForm({ navigation }) {
+export default function SignOnForm({ route, navigation }) {
+    const myContext = useContext(AppContext); //get global vars
+    const {raceName, raceDate} = route.params;  //get navigation parameters
+
+    const[boatName, setBoatName] = useState();
+    const[helmName, setHelmName] = useState(myContext.userName);
+    const[crewName, setCrewName] = useState();
+
     return (
     <SafeAreaView style={styles.container}>
         <AppBarComponent title="Sign On" disabled={false}/>
-        <TextInput
-          label="Boat"
-          placeholder="Enter Boat"
-          //value={}
-          //onChangeText={}
-          autoComplete="on"
-          style={styles.input}
-          mode="outlined"
-        />
-        <Button mode="contained" onPress={console.log("send sign on")} style={styles.button} labelStyle={{fontSize: 20}}>
-          Login
-        </Button>
+        <ScrollView>
+          <CustomSubHeader>Sign On - {raceDate}, {raceName}:</CustomSubHeader>
+          <TextInput
+            label="Boat Name"
+            placeholder="Enter Boat Name"
+            value={boatName}
+            onChangeText={() => setBoatName(boatName)}
+            style={styles.input}
+            mode="outlined"
+          />
+          <TextInput
+            label="Helm Name"
+            placeholder="Enter Helm Name"
+            value={helmName}
+            onChangeText={() => setHelmName(helmName)}
+            style={styles.input}
+            mode="outlined"
+          />
+          <TextInput
+            label="Boat Name"
+            placeholder="Enter Boat Name"
+            value={crewName}
+            onChangeText={() => setCrewName(crewName)}
+            style={styles.input}
+            mode="outlined"
+          />
+          <Button mode="contained" onPress={console.log("send sign on")} style={styles.button} labelStyle={{fontSize: 20}}>
+            Login
+          </Button>
+        </ScrollView>
     </SafeAreaView>
     )
 }
